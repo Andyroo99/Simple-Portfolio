683990f6c3a4dc1eea1c4cbd6b610b67a54f7908
Andrew Muston <99222760+Andyroo99@users.noreply.github.com>
2023-06-18 15:09:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-$(window).on('resize load scroll', function() {
-  var windowHeight = $(window).height();
+$(window).on('resize load', function() {
   var windowWidth = $(window).width();
-  var aspectRatio = 1080 / 1920; // Assuming the original image size is 1920x1080
+  var aspectRatio = 1920 / 1080; // Assuming the original image size is 1920x1080
 
-  var targetHeight = windowWidth * aspectRatio;
-  if (targetHeight > windowHeight) {
-    targetHeight = windowHeight;
-  }
+  var targetWidth = windowWidth;
+  var targetHeight = windowWidth / aspectRatio;
 
+  $('.background-image').width(targetWidth);
   $('.background-image').height(targetHeight);
 
-  var textFontSize = windowWidth * 0.05; // Adjust the factor (0.03) as desired for the font size
+  var textFontSize = windowWidth * 0.05; // Adjust the factor (0.05) as desired for the font size
 
   if (windowWidth >= 1900) {
     textFontSize = 1350 * 0.09; // Set a fixed font size for width <= 1350
   }
 
-  $('.text-overlay p').css('font-size', textFontSize);
+  if (windowWidth <= 750) {
+    textFontSize = textFontSize * 2; // Twice the font size when width <= 750
+  }
+
+  $('.text-overlay h1').css('font-size', textFontSize);
+  $('.text-overlay p').css('font-size', textFontSize * 0.5); // Adjust the factor (0.5) as desired for the sub-header font size
 
   if ($(window).scrollTop() > 10) {
     $('.back-to-top').removeClass('hidden'); // Remove the 'hidden' class to show the button
@@ -24,6 +27,7 @@ $(window).on('resize load scroll', function() {
     $('.back-to-top').addClass('hidden'); // Add the 'hidden' class to hide the button
   }
 });
+
 
 $(document).ready(function() {
   // Scroll to top when the back-to-top button is clicked
